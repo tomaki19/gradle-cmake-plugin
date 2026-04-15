ace091694e94a6cded9b8dec29184f10dbcb1e07
Thomas Killer <tkone@gmx.ch>
2026-04-15 12:22:29 +0200

    fix Non-deterministic HashSet iteration for build configs

--- src/main/java/io/github/tomaki19/gradle/cmake/extension/api/CMakeBuildConfigs.java
+++ src/main/java/io/github/tomaki19/gradle/cmake/extension/api/CMakeBuildConfigs.java
@@ -6,15 +6,15 @@ package io.github.tomaki19.gradle.cmake.extension.api;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class CMakeBuildConfigs {
 
-  private Set<String> entries = new HashSet<>(Arrays.asList("Debug", "Release", "RelWithDebInfo", "MinSizeRel"));
+  private Set<String> entries = new TreeSet<>(Arrays.asList("Debug", "Release", "RelWithDebInfo", "MinSizeRel"));
 
   public void set(final String... entries) {
-    this.entries = new HashSet<>(Arrays.asList(entries));
+    this.entries = new TreeSet<>(Arrays.asList(entries));
   }
 
   public Set<String> get() {
